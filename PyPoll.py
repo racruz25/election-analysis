bdf653f00b935c4d27fcabc951aaf47cde6a2293
racruz25 <91695473+racruz25@users.noreply.github.com>
2021-11-12 19:11:07 -0600

    Module 3.5.3

--- PyPoll.py
+++ PyPoll.py
@@ -17,6 +17,9 @@ total_votes = 0
 # Declare new list of candidates
 candidate_options = []
 
+# Declare the empty dictionary.
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -36,11 +39,18 @@ with open(file_to_load) as election_data:
 
         # If the candidate does not match any existing candidate...
         if candidate_name not in candidate_options:
+           
             # Add it to the list of candidates.
             candidate_options.append(candidate_name)
 
-# Print the candidate list.
-print(candidate_options)
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+# Print the candidate vote dictionary.
+print(candidate_votes)
 
 # 2. A complete list of candidates who received votes.
 # 3. The percentage of votes each candidate won.
