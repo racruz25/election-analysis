9609d6b55d937f37ed81efdb935e27aeddc99605
racruz25 <91695473+racruz25@users.noreply.github.com>
2021-11-12 18:41:08 -0600

    Module 3.5.1

--- PyPoll.py
+++ PyPoll.py
@@ -9,17 +9,28 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. The Total number of votes cast.
+
+# Initialize a total vote counter.
+total_votes = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
 
-# 1. The Total number of votes cast.
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+# Print the total votes.
+print(total_votes)
+
 # 2. A complete list of candidates who received votes.
 # 3. The percentage of votes each candidate won.
 # 4. The total number of votes each candidate won.
