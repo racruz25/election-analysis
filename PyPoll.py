61b6431d859cf21e205d3197ee03e5645ca12f06
racruz25 <91695473+racruz25@users.noreply.github.com>
2021-11-12 19:02:46 -0600

    Module 3.5.2

--- PyPoll.py
+++ PyPoll.py
@@ -14,6 +14,9 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Initialize a total vote counter.
 total_votes = 0
 
+# Declare new list of candidates
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -28,8 +31,16 @@ with open(file_to_load) as election_data:
         # Add to the total vote count.
         total_votes += 1
 
-# Print the total votes.
-print(total_votes)
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+# Print the candidate list.
+print(candidate_options)
 
 # 2. A complete list of candidates who received votes.
 # 3. The percentage of votes each candidate won.
